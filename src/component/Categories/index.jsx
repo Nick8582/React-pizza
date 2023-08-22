692ccdfd7fc3a40c27144b341f3ead2b17b97a92
Vladislav Potapov <89172111013q@gmail.com>
2023-08-22 12:50:17 +0300

    create: components 'Header', 'Categories', 'Sort'

---
+++ src/component/Categories/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Categories(props) {
+  return (
+    <div class="categories">
+      <ul>
+        <li class="active">Все</li>
+        <li>Мясные</li>
+        <li>Вегетарианская</li>
+        <li>Гриль</li>
+        <li>Острые</li>
+        <li>Закрытые</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Categories;

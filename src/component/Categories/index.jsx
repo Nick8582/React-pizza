b5bd374563da00bc35e29782835a2914139c0ea7
Vladislav Potapov <89172111013q@gmail.com>
2023-08-22 23:30:49 +0300

    add: output of an array of categories and product cards

--- src/component/Categories/index.jsx
+++ src/component/Categories/index.jsx
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function Categories(props) {
+  const [activeIndex, setActiveIndex] = useState(0)
+
+  const categories = [
+    'Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые'
+  ]
+  const onClickCategory = (index) => {
+    setActiveIndex(index)
+  }
+
   return (
-    <div class="categories">
+    <div className="categories">
       <ul>
-        <li class="active">Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
+        {categories.map((item, index) => (
+          <li
+            onClick={() => onClickCategory(index)}
+            className={activeIndex === index ? 'active' : ''}
+            key={index}>{item}</li>
+        ))}
       </ul>
     </div>
   );

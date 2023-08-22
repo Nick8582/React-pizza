5589c32631bafdf3fd6fd565d92cefd95aae0aa5
Vladislav Potapov <89172111013q@gmail.com>
2023-08-22 23:52:07 +0300

    add: selecting the parameters of the added product

--- src/component/PizzaBlock/index.jsx
+++ src/component/PizzaBlock/index.jsx
@@ -1,40 +1,47 @@
-import {useState} from 'react';
+import {useState} from "react";
 
-function Index({imageUrl, title, price}) {
-  const [pizzaCount, setPizzaCount] = useState(0)
+function Index({imageUrl, title, price, sizes, types}) {
+  const typeNames = ['тонкое', 'традиционное']
 
-  const onClickAddPizza = () => {
-    setPizzaCount(pizzaCount + 1)
-  }
+  const [activeType, setActiveType] = useState(types[0])
+  const [activeSize, setActiveSize] = useState(0)
 
   return (
     <div className="pizza-block">
       <img
         className="pizza-block__image"
         src={imageUrl}
-        alt="Pizza"
+        alt={`Картинка ${title}`}
       />
       <h4 className="pizza-block__title">{title}</h4>
       <div className="pizza-block__selector">
         <ul>
-          <li className="active">тонкое</li>
-          <li>традиционное</li>
+          {types.map((typeId) => (
+            <li
+              className={activeType === typeId ? "active" : ""}
+              onClick={() => setActiveType(typeId)}
+              key={typeId}>{typeNames[typeId]}</li>
+          ))}
         </ul>
         <ul>
-          <li className="active">26 см.
-          </li>
-          <li>30 см.</li>
-          <li>40 см.</li>
+          {sizes.map((item, index) => (
+            <li
+              className={activeSize === index ? "active" : ''}
+              onClick={() => setActiveSize(index)}
+              key={index}>{item} см.</li>
+          ))}
         </ul>
       </div>
       <div className="pizza-block__bottom">
         <div className="pizza-block__price">от {price} ₽</div>
-        <button onClick={onClickAddPizza} className="button button--outline button--add"  >
+        <button className="button button--outline button--add">
           <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z" fill="white"/>
+            <path
+              d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z"
+              fill="white"/>
           </svg>
           <span>Добавить</span>
-          {pizzaCount > 0 && <i>{pizzaCount}</i>}
+          <i>0</i>
         </button>
       </div>
     </div>

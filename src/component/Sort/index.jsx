292b7ae622db64442b4c131efa8fcb4bff6ec62c
Vladislav Potapov <89172111013q@gmail.com>
2023-08-28 12:16:48 +0300

    add: outside click to component Sort

--- src/component/Sort/index.jsx
+++ src/component/Sort/index.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import {useDispatch, useSelector} from "react-redux";
 import {setSortByTo, setSortType} from "../../redux/slices/filterSlice";
 
@@ -12,6 +12,7 @@ function Sort() {
   const dispatch = useDispatch()
   const sort = useSelector(state => state.filter.sort)
   const sortByTo = useSelector(state => state.filter.sortByTo)
+  const sortRef = useRef();
 
   const [open, setOpen] = useState(false)
 
@@ -24,10 +25,25 @@ function Sort() {
     dispatch(setSortByTo(item))
   }
 
+  useEffect(() => {
+    const handleClickOutside = (e) => {
+      if(!e.composedPath().includes(sortRef.current)) {
+        setOpen(false)
+      }
+    }
+
+    document.body.addEventListener('click', handleClickOutside)
+
+    return () => {
+      document.body.removeEventListener('click', handleClickOutside)
+    }
+  }, []);
+
   return (
-    <div className="sort">
+    <div ref={sortRef} className="sort">
       <div className="sort__label">
-        <svg onClick={() => onChangeSortByTo(!sortByTo)} className={sortByTo ? 'sort__arrow-rotate' : ''} width="10" height="6" viewBox="0 0 10 6" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <svg onClick={() => onChangeSortByTo(!sortByTo)} className={sortByTo ? 'sort__arrow-rotate' : ''} width="10"
+             height="6" viewBox="0 0 10 6" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path
             d="M10 5C10 5.16927 9.93815 5.31576 9.81445 5.43945C9.69075 5.56315 9.54427 5.625 9.375 5.625H0.625C0.455729 5.625 0.309245 5.56315 0.185547 5.43945C0.061849 5.31576 0 5.16927 0 5C0 4.83073 0.061849 4.68424 0.185547 4.56055L4.56055 0.185547C4.68424 0.061849 4.83073 0 5 0C5.16927 0 5.31576 0.061849 5.43945 0.185547L9.81445 4.56055C9.93815 4.68424 10 4.83073 10 5Z"
             fill="#2C2C2C"/>
